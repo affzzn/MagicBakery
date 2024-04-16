c7da955d904b23f06b62307b8d0d06eedaf59282
Affan Imran <affanimran@Affans-MacBook-Air.local>
2024-04-16 11:08:17 +0100

    console utils ig

--- src/main/util/ConsoleUtils.java
+++ src/main/util/ConsoleUtils.java
@@ -10,29 +10,56 @@ public class ConsoleUtils {
 
     public ConsoleUtils() {
         console = System.console();
+        // this.console = console; //
     }
 
     public String readLine() {
-        return "";
+        return console.readLine();
     }
 
     public String readLine(String fmt, Object... args) {
-        return "";
+        return console.readLine(fmt, args);
     }
 
     public File promptForFilePath(String prompt) {
-        return null;
+
+        String path = console.readLine(prompt);
+        File fileObj = new File(path);
+
+        return fileObj;
     }
 
     public ArrayList<String> promptForNewPlayers(String prompt) {
-        return null;
+
+        ArrayList<String> players = new ArrayList<String>();
+
+        int minPlayers = 2;
+        int maxPlayers = 5;
+
+        int numPlayers = 0;
+
+        //
+        while (numPlayers < minPlayers || numPlayers > maxPlayers) {
+            numPlayers = Integer.parseInt(console.readLine(prompt));
+            if (numPlayers < minPlayers || numPlayers > maxPlayers) {
+                System.out.println("Please enter a number between 2 and 5");
+
+            }
+        }
+
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println("Enter player name: ");
+            players.add(console.readLine());
+        }
+
+        return players;
     }
 
     public boolean promptForStartLoad(String prompt) {
-        return false;
+        return console.readLine(prompt).equalsIgnoreCase("yes");
     }
 
     public boolean promptForYesNo(String prompt) {
-        return false;
+        return console.readLine(prompt).equalsIgnoreCase("yes");
     }
 }
