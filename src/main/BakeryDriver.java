63b45ca65189babd0ef8b71b01e651de3e7c668b
Affan Imran <affanimran@Affans-MacBook-Air.local>
2024-04-16 20:10:03 +0100

    pls push

--- src/main/BakeryDriver.java
+++ src/main/BakeryDriver.java
@@ -19,18 +19,5 @@ public class BakeryDriver {
 
     public static void main(String[] args) throws IOException {
 
-        // MagicBakery magicBakery = new MagicBakery("../../io/ingredients.csv",
-        // "../../io/layers.csv");
-
-        // ArrayList<Ingredient> ingredients = CardUtils
-        // .readIngredientFile("io/ingredients.csv");
-
-        // ArrayList<Layer> layers = CardUtils
-        // .readLayerFile("io/layers.csv");
-
-        // ArrayList<String> gamePlayers = ConsoleUtils.promptForNewPlayers("Enter
-        // number of players:");
-
-        // magicBakery.startGame(gamePlayers, "io/customers.csv");
     }
 }
